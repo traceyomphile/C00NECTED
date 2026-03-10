320716afdd53760c3552e6da00cd347d3faab230
traceyomphile <omphiletracey@gmail.com>
2026-03-10 13:38:02 +0200

    Added a column for last_seen in users table

--- infrastructure.py
+++ infrastructure.py
@@ -1,15 +1,28 @@
 import sqlite3
 import fakeredis
+import threading
 from google.cloud import storage
 
+DB_FILE = "arcp.db"
 
-# ------- SQLITE WITH CONcursorRENCY
-def get_connection(db_file="arcp.db") -> sqlite3.Connection:
+# Global lock for write safety
+db_lock = threading.Lock()
+
+# ------- SQLITE WITH CONCURRENCY
+def get_connection(db_file: str = DB_FILE) -> sqlite3.Connection:
     # Intialise database to wait for 15 seconds if db locked by another write
-    conn = sqlite3.connect(db_file, timeout=15)
+    conn = sqlite3.connect(
+        db_file, timeout=15,
+        check_same_thread=False     # Allow access across threads
+    )
 
-    # Enable multiple readers and 1 write
-    conn.execute("PRAGMA journal+mode=WAL;")
+    conn.row_factory = sqlite3.Row
+
+    # Enable WAL mode (best for concurrent read/write)
+    conn.execute("PRAGMA journal_mode=WAL;")
+
+    # Wait before throwing "database is locked"
+    conn.execute("PRAGMA busy_timeout=15000;")
 
     return conn
 
@@ -33,24 +46,27 @@ def initialise_database():
 
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
+            password TEXT NOT NULL,
+            last_seen INTEGER
         )
         """)
 
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS groups (
-            id SERIAL PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             group_id TEXT UNIQUE NOT NULL
         )
         """)
 
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS group_members (
-            group_id TEXT REFERENCES groups(group_id),
-            username TEXT REFERENCES users(username),
-            PRIMARY KEY (group_id, username)
+            group_id TEXT NOT NULL,
+            username TEXT NOT NULL,
+            PRIMARY KEY (group_id, username),
+            FOREIGN KEY(group_id) REFERENCES groups(group_id),
+            FOREIGN KEY(username) REFERENCES users(username),
         )
         """)
 
@@ -64,3 +80,6 @@ def initialise_database():
     finally:
         if conn:
            conn.close()
+
+def get_db():
+    return get_connection()
